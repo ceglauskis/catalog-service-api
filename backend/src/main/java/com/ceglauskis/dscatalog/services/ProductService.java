0bfb28513b08f00c258a58a3a8461bc6d85ba7b9
maikoceglauskis@gmail.com <maikoceglauskis@gmail.com>
2026-02-02 22:18:38 -0300

    CRUD Product

--- backend/src/main/java/com/ceglauskis/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/ceglauskis/dscatalog/services/ProductService.java
@@ -1,8 +1,10 @@
 package com.ceglauskis.dscatalog.services;
 
 import com.ceglauskis.dscatalog.dto.CategoryDTO;
+import com.ceglauskis.dscatalog.dto.ProductDTO;
 import com.ceglauskis.dscatalog.entities.Category;
-import com.ceglauskis.dscatalog.repositories.CategoryRepository;
+import com.ceglauskis.dscatalog.entities.Product;
+import com.ceglauskis.dscatalog.repositories.ProductRepository;
 import com.ceglauskis.dscatalog.services.exceptions.DatabaseException;
 import com.ceglauskis.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
@@ -11,53 +13,56 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
-public class CategoryService {
+public class ProductService {
 
     @Autowired
-    private CategoryRepository repository;
+    private ProductRepository repository;
+
 
     @Transactional(readOnly = true)
-    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){;
-        Page<Category> page = repository.findAll(pageRequest);
-        return page.map(x -> new CategoryDTO(x));
+    public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Product> page = repository.findAll(pageRequest);
+        return page.map(x -> new ProductDTO(x));
     }
 
     @Transactional(readOnly = true)
-    public CategoryDTO findById(Long id){
-        Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-        return new CategoryDTO(entity);
+    public ProductDTO findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new ProductDTO(entity);
     }
 
-    @Transactional
-    public CategoryDTO insert(CategoryDTO dto) {
-        Category entity = new Category();
+    @Transactional(readOnly = true)
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
         entity.setName(dto.getName());
-        entity = repository.save(entity);
-        return new CategoryDTO(entity);
+        entity.setPrice(dto.getPrice());
+        entity.setDescription(dto.getDescription());
+        entity.setImgUrl(dto.getImgUrl());
+        return new ProductDTO(entity);
     }
 
-    @Transactional
-    public CategoryDTO update(Long id, CategoryDTO dto) {
-        try {
-            Category entity = repository.getReferenceById(id);
+    @Transactional(readOnly = true)
+    public ProductDTO update(Long id, ProductDTO dto) {
+        try{
+            Product entity = repository.getReferenceById(id);
             entity.setName(dto.getName());
+            entity.setPrice(dto.getPrice());
+            entity.setDescription(dto.getDescription());
+            entity.setImgUrl(dto.getImgUrl());
             entity = repository.save(entity);
-            return new CategoryDTO(entity);
-        }catch (EntityNotFoundException e){
+            return new ProductDTO(entity);
+        } catch (EntityNotFoundException e){
             throw new ResourceNotFoundException("Id not found: " + id);
         }
     }
 
-    @Transactional(propagation = Propagation.SUPPORTS)
+    @Transactional(readOnly = true)
     public void delete(Long id) {
         if (!repository.existsById(id)) {
             throw new ResourceNotFoundException("Id not found: " + id);
